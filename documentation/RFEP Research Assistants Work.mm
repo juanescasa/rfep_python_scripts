a4b504672421bfd892aec42cbc0562e1f309d16e
Jecs <juanescasa@gmail.com>
2021-09-14 20:33:30 +1000

    I added documentation to tableu Data Reading analysis and created a freeplane to serve as notebook of the Research Assistants work

---
+++ documentation/RFEP Research Assistants Work.mm
@@ -0,0 +1,111 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="RFEP RA work" FOLDED="false" ID="ID_1500781255" CREATED="1631613803875" MODIFIED="1631613825808" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Projects" POSITION="right" ID="ID_1617812489" CREATED="1631613827109" MODIFIED="1631613830757">
+<edge COLOR="#ff0000"/>
+<node TEXT="Improve Performance of Data Reading" ID="ID_1091002594" CREATED="1631601371096" MODIFIED="1631613953442">
+<icon BUILTIN="idea"/>
+</node>
+<node TEXT="Modify the Generation of Scenario Procedure" ID="ID_1480486737" CREATED="1631601379984" MODIFIED="1631613953442">
+<icon BUILTIN="idea"/>
+</node>
+<node TEXT="Improve Performance of Export Results" ID="ID_1765671147" CREATED="1631601402776" MODIFIED="1631601429052"/>
+</node>
+<node TEXT="Meetings" POSITION="right" ID="ID_1973362605" CREATED="1631613831117" MODIFIED="1631613836838">
+<edge COLOR="#0000ff"/>
+<node TEXT="210915 Meeting Research Assistants" ID="ID_43151260" CREATED="1631601357232" MODIFIED="1631601367727">
+<node TEXT="Agenda" ID="ID_463215484" CREATED="1631601441352" MODIFIED="1631601515628">
+<node TEXT="Introduction" ID="ID_146927984" CREATED="1631601517666" MODIFIED="1631601519399"/>
+<node TEXT="What did you do last week?" ID="ID_1134307678" CREATED="1631601576304" MODIFIED="1631601582580"/>
+<node TEXT="Using GitHub" FOLDED="true" ID="ID_592929993" CREATED="1631601443151" MODIFIED="1631601447530">
+<node TEXT="Create Repository of the project" ID="ID_1335864086" CREATED="1631602900177" MODIFIED="1631602906967"/>
+<node TEXT="Create a branch" ID="ID_176533212" CREATED="1631602971578" MODIFIED="1631602974181"/>
+<node TEXT="Update one of the files to work with relative paths" ID="ID_1755767579" CREATED="1631602907283" MODIFIED="1631602925516"/>
+<node TEXT="Test the functionality" ID="ID_1689930819" CREATED="1631602962761" MODIFIED="1631602987403"/>
+<node TEXT="Commit the change" ID="ID_293721152" CREATED="1631602987569" MODIFIED="1631602994142"/>
+<node TEXT="Push the change" ID="ID_1925255984" CREATED="1631602994329" MODIFIED="1631602996590"/>
+</node>
+<node TEXT="Projects Definition" FOLDED="true" ID="ID_1440975294" CREATED="1631601367960" MODIFIED="1631601506155">
+<node TEXT="Improve Performance of Data Reading" ID="ID_1745766013" CREATED="1631601371096" MODIFIED="1631601378042"/>
+<node TEXT="Modify the Generation of Scenario Procedure" ID="ID_960002524" CREATED="1631601379984" MODIFIED="1631601393426"/>
+<node TEXT="Improve Performance of Export Results" ID="ID_1951335957" CREATED="1631601402776" MODIFIED="1631601429052"/>
+</node>
+<node TEXT="Introduction to the data model of the RFEP" FOLDED="true" ID="ID_1531326635" CREATED="1631613995926" MODIFIED="1631614008375">
+<node TEXT="How the data is generated" FOLDED="true" ID="ID_1562655601" CREATED="1631614364645" MODIFIED="1631614369191">
+<node TEXT="Excel" ID="ID_565389431" CREATED="1631614369485" MODIFIED="1631614372503"/>
+<node TEXT="Alteryx" ID="ID_1271082592" CREATED="1631614372678" MODIFIED="1631614375287"/>
+<node TEXT="Generator Tables" ID="ID_141511250" CREATED="1631614381909" MODIFIED="1631614386808"/>
+<node TEXT="Data Generation Procedure" ID="ID_380326402" CREATED="1631614407991" MODIFIED="1631614416848"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Agreements" POSITION="right" ID="ID_1353726954" CREATED="1631613901121" MODIFIED="1631613903434">
+<edge COLOR="#00ff00"/>
+</node>
+</node>
+</map>
