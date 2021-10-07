1d1fbbedb6b8bbd5b89d96a2aca5887b8f3fc53c
alejandro <alejandro9.ch321@gmail.com>
2021-10-06 21:47:19 -0500

    Probando con LFS

--- models/xd.py
+++ models/xd.py
@@ -1,13 +1,3 @@
-import pandas as pd
-#Read generator tables
-folder_path = "data\\"
-
-#Each scenario generator change this
-folder_name = "Path 2000"
-folder_parent = folder_path + folder_name + '\\'
-
-file_name = "Scenario Map.xlsx"
-file = folder_parent + file_name
-sh = 'Generate Tables'
-df_scenario_map = pd.read_excel(file, sheet_name = sh)
-print(df_scenario_map.shape)
+import numpy as np
+x = np.array(["hola","gay"])
+print(x)
